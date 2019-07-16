17e125e6141abadbad0cbfa04b5fc07040ded6f3
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2019-07-16 11:29:47 +0200

    Reduce environment to a minimum

--- backend/src/main/kotlin/de/saio/dms/api/DmsResource.kt
+++ backend/src/main/kotlin/de/saio/dms/api/DmsResource.kt
@@ -1,12 +1,17 @@
 package de.saio.dms.api
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
+@RequestMapping("/api")
 class DmsResource {
 
-    @GetMapping
-    fun hello () = "foobar"
+    @GetMapping("/hello")
+    fun hello () = Foo("lol")
 
 }
+
+data class Foo(val value: String)
---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+<nav class="bg-white px-8 pt-2 my-2 border-b">
+    <div class="-mb-px flex relative">
+        <router-link
+                exact
+                to="/"
+                active-class="border-blue-600 hover:border-blue-600"
+                class="no-underline text-center block text-blue-600 font-medium mr-3 border-b-2 pt-2 pb-3 px-3 inline-block hover:border-blue-900 hover:text-blue-900 border-transparent">
+            Home
+        </router-link>
+        <router-link
+                to="/phrases/search"
+                active-class="border-blue-600 hover:border-blue-600"
+                class="no-underline text-center block text-blue-600 font-medium mr-3 border-b-2 pt-2 pb-3 px-3 inline-block hover:border-blue-900 hover:text-blue-900 border-transparent">
+            Search Phrase
+        </router-link>
+        <router-link
+                to="/phrases/add"
+                active-class="border-blue-600 hover:border-blue-600"
+                class="no-underline text-center block text-blue-600 font-medium mr-3 border-b-2 pt-2 pb-3 px-3 inline-block hover:border-blue-900 hover:text-blue-900 border-transparent">
+            Add Phrase
+        </router-link>
+        <router-link
+                exact
+                to="/phrases/popular"
+                active-class="border-blue-600 hover:border-blue-600"
+                class="no-underline text-center block text-blue-600 font-medium mr-3 border-b-2 pt-2 pb-3 px-3 inline-block hover:border-blue-900 hover:text-blue-900 border-transparent">
+            Popular
+        </router-link>
+    </div>
+</nav>
+</template>
