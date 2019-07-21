ceedc3f2c8950ea3e24425c636ba72cbed9c826e
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2019-07-21 17:40:55 +0200

    Reorder items

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
-import {store} from './store/store';
-import App from './App.vue';
-import {routes} from './routes';
-import vueDebounce from 'vue-debounce'
 import VueTailwind from './VueTailwind/plugin';
 import Notifications from 'vue-notification'
+import {store} from './store/store';
+import {routes} from './routes';
+import App from './App.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(VueTailwind);
-Vue.use(vueDebounce);
 Vue.use(Notifications);
 
 const router = new VueRouter({
