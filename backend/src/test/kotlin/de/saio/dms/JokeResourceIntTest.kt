80284a0cd9556e02a668f3912fa119e8d49c876e
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2019-07-28 16:00:41 +0200

    Remove unused imports

--- backend/src/test/kotlin/de/saio/dms/JokeResourceIntTest.kt
+++ backend/src/test/kotlin/de/saio/dms/JokeResourceIntTest.kt
@@ -4,17 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.util.*
 
 @WebMvcTest(JokeResource::class)
